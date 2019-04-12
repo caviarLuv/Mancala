4e37689cc6a7cc6e9a054c9e9903a99476c4e278
S_HE <stanhe@gmail.com>
2019-04-12 14:07:11 -0700

    Update Data.Class/java

--- src/Data.java
+++ src/Data.java
@@ -1,14 +1,22 @@
+import java.util.ArrayList;
 
-
+import javax.swing.event.*;
 
 public class Data {
 	
 	//who's turn is it
 	private boolean isPlayerA;
+	private boolean gameEnd;
+	private boolean canUndo;
 	
 	//the pits(0-5) and the mancala(6) 
 	private int[] playerA;
 	private int[] playerB;
+	
+	private int[] undoA;
+	private int[] undoB;
+	
+	private ArrayList<ChangeListener> listeners;
 	
 	/**
 	 * Constructs a new Data object, filling each pit with the specified amount of stones
@@ -18,12 +26,16 @@ public class Data {
 	{
 		playerA = new int[7];
 		playerB = new int[7];
+		listeners = new ArrayList<>();
 		isPlayerA = true;
+		gameEnd = false;
 		for(int i = 0; i < 6; i++)
 		{
 			playerA[i] = stones;
 			playerB[i] = stones;
 		}
+		undoA = playerA;
+		undoB = playerB;
 	}
 	
 	
@@ -77,26 +89,48 @@ public class Data {
 			{
 				if(side == playerA)
 				{
-					int addStone = playerB[6-pit];
-					playerB[6-pit] = 0;
-					side[6] += addStone;
+					capture(side, playerB, pit);
 				}
 				else if(side == playerB)
 				{
-					int addStone = playerA[6-pit];
-					playerA[6-pit] = 0;
-					side[6] += addStone;
+					capture(side, playerA, pit);
 				}
 			}
 			//if the last stone goes into current player's mancala
 			else if(pit >= 7 && side[pit-1] == 1)
 			{
-				//take an extra turn
+				isPlayerA = !isPlayerA;
 			}
+			isPlayerA = !isPlayerA;
 		}
-		//update view
-		//insert code here//
+		
+		if(isEmpty(playerA))
+		{
+			gameEndProtocal(playerB);
+		}
+		else if(isEmpty(playerB))
+		{
+			gameEndProtocal(playerA);
+		}
+		
+		
+		ChangeEvent event = new ChangeEvent(this);
+		for(ChangeListener l: listeners)
+		{
+			l.stateChanged(event);
+		}
 	}
+	
+	
+	/**
+	 * Adds a changelistener to the listeners of the data class
+	 * @param listener the listener to add
+	 */
+	public void addChangeListener(ChangeListener listener)
+	{
+		listeners.add(listener);
+	}
+	
 	
 	/**
 	 * Gets value of isPlayerA
@@ -105,6 +139,38 @@ public class Data {
 	public boolean getIsPLayerA()
 	{
 		return isPlayerA;
+	}
+	
+	public void capture(int[] cSide, int[] oppSide, int pit)
+	{
+		cSide[pit-1] = 0;
+		int addStone = oppSide[6-pit] + 1;
+		oppSide[6-pit] = 0;
+		cSide[6] += addStone;
+	}
+	
+	public void gameEndProtocal(int[] nonEmpty)
+	{
+		for(int i = 0; i < nonEmpty.length-1; i++)
+		{
+			nonEmpty[nonEmpty.length-1] += nonEmpty[i];
+			nonEmpty[i] = 0;
+		}
+		gameEnd = true;
+	}
+
+	public boolean isEmpty(int[] test)
+	{
+		boolean empty = true;
+		int counter = 0;
+		while(empty && counter < test.length-1)
+		{
+			if(test[counter] != 0 )
+			{
+				empty = false;
+			}
+		}
+		return empty;
 	}
 	
 }
