9238994d7d2305092911f964810aa9b3aeb74db0
Julia Chin <juliachin8@gmail.com>
2019-04-19 15:36:08 -0700

    Added Style Frame

--- src/Frame.java
+++ src/Frame.java
@@ -5,12 +5,9 @@ import javax.swing.JFrame;
 public class Frame extends JFrame {
 	private Board b;
 	public Frame(Data model) {
-<<<<<<< HEAD
 		
 		this.setTitle("Mancala");
-=======
 		setSize(400,300);
->>>>>>> upstream/master
 		this.setResizable(false);
 		b = new Board(this.getSize(), model);
 		
--- src/MancalaTester.java
+++ src/MancalaTester.java
@@ -1,14 +1,9 @@
 
 public class MancalaTester {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
-		Data model = new Data(4); 
+		Data model = new Data(4); //intial user input num of stone
 		GameStartFrame gameStart = new GameStartFrame(model); //initial user input num of stone
-=======
-		
-		Data model = new Data(4);  //initial user input num of stone
->>>>>>> upstream/master
 		Frame frame = new Frame(model);
 		model.addChangeListener(frame.getBoard());
 	}
