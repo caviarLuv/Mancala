9d4960c562db9c974cb1edb3b1826ca5d3409d3f
caviarLuv <yashleycaviar@gmail.com>
2019-04-19 15:04:23 -0700

    updated

--- src/MancalaTester.java
+++ src/MancalaTester.java
@@ -1,9 +1,9 @@
 
 public class MancalaTester {
 	public static void main(String[] args) {
-		
-		GameStartFrame gameStart = new GameStartFrame();
-		Data model = new Data(4);  //initial user input num of stone
+
+		Data model = new Data(4); 
+		GameStartFrame gameStart = new GameStartFrame(model); //initial user input num of stone
 		Frame frame = new Frame(model);
 		model.addChangeListener(frame.getBoard());
 	}
