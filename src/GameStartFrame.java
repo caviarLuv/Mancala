e854616e1caf7099cf18ec796adf61fcc20cafb5
Julia Chin <juliachin8@gmail.com>
2019-04-19 17:02:24 -0700

    Updated Style Frame

--- src/GameStartFrame.java
+++ src/GameStartFrame.java
@@ -1,18 +1,18 @@
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 public class GameStartFrame extends JFrame{
 	private Data d;
 	private int initialStones;
 	public GameStartFrame(Data d) {
 		this.d = d;
 		final Container contentPane = this.getContentPane();
-		this.setLocation(500, 0);
-		this.setTitle("Option");
 		initialStones = 0;
 		
 		JButton three = new JButton("3 stones");
@@ -35,10 +35,22 @@ public class GameStartFrame extends JFrame{
 			}
 			
 		});
+
+		JPanel stones = new JPanel();
+		stones.setLayout(new BoxLayout(stones, BoxLayout.X_AXIS));
+		stones.add(three);
+		stones.add(four);
+		
+		JPanel instruction = new JPanel();
+		JTextArea prompt = new JTextArea("Choose the # of stones per pit");
+		prompt.setEditable(false);
+		instruction.add(prompt);
+		instruction.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
 		this.setVisible(true);
-		this.setLayout(new BoxLayout(contentPane, BoxLayout.PAGE_AXIS));
-		this.add(three);
-		this.add(four);
+		this.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		this.add(instruction);
+		this.add(stones);
 		this.pack();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
--- src/StyleFrame.java
+++ src/StyleFrame.java
@@ -1,4 +1,60 @@
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class StyleFrame {
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+
+public class StyleFrame extends JFrame{
+	
+	private Data d;
+	
+	public StyleFrame(Data d){
+		this.d = d;
+		final Container contentPane = this.getContentPane();
+	
+		JButton StyleA = new JButton("Style A");
+		JButton StyleB = new JButton("Style B");
+	
+		JPanel instruction = new JPanel();
+		JTextArea prompt = new JTextArea("Select a game theme");
+		prompt.setEditable(false);
+		instruction.add(prompt);
+		instruction.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		JPanel styles = new JPanel();
+		styles.setLayout(new BoxLayout(styles, BoxLayout.X_AXIS));
+		styles.add(StyleA);
+		styles.add(StyleB);
+		
+		this.setVisible(true);
+		this.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		this.add(instruction);
+		this.add(styles);
+		this.pack();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+//		if(StyleA.getModel().isPressed() || StyleB.getModel().isPressed()){
+//			if(StyleA.getModel().isPressed()){
+//				
+//			}
+//			if(StyleA.getModel().isPressed()){
+//							
+//			}
+//			this.addWindowListener(new WindowAdapter(){
+//				public void windowClosing(WindowEvent e){
+//					
+//					
+//				}
+//			});
+//		}
+	}
+	
+	
 
 }
