f035186f5f1fc0f4811edce73efc34daec47bd6b
caviarLuv <yashleycaviar@gmail.com>
2019-04-26 15:39:53 -0700

    modified stone display

--- src/Pit.java
+++ src/Pit.java
@@ -1,4 +1,5 @@
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
@@ -13,6 +14,8 @@ public class Pit implements Part{
 	private int stones;
 	private String label;
 	private ArrayList<ChangeListener> cl;
+	
+	public static final int RADIUS = 10;
 	public Pit(int x, int y, int h, int k, int initialStone, String label) {
 		this.x = x;
 		this.y = y;
@@ -27,7 +30,21 @@ public class Pit implements Part{
 	public void draw(Graphics2D g2) {
 		Rectangle2D.Double p = new Rectangle2D.Double(x, y, h, k);
 		g2.drawString(label, x+h/2, y+k);
-		g2.drawString(stones + "", x+h/2, y+k/2);
+		g2.drawString(stones + "", x+h/2, y+k/2); 
+		
+		int stoneY = this.y;
+		int stoneX = this.x;
+		for(int i = 0; i<stones; i++) {
+			Ellipse2D.Double s = new Ellipse2D.Double(stoneX,stoneY,RADIUS,RADIUS);
+			if(stoneX+RADIUS*2 >= h) {
+				stoneY += RADIUS;
+				stoneX = this.x;
+			}
+			else {
+				stoneX += RADIUS;
+			}
+			g2.fill(s);
+		}
 		g2.draw(p);
 	}
 
